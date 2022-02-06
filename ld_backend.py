219d791dc92ce3f99d6c37d2d2eecc01ad7d401f
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2022-02-06 00:29:24 +0000

    Fixed missing protocol

--- ld_backend.py
+++ ld_backend.py
@@ -27,7 +27,7 @@ def hello():
 @app.route('/declarative-linter')
 def linter():
     filename = request.args.get("filename", default=JENKINS_DIR + "/Jenkinsfile", type=str)
-    command = "java -jar jenkins-cli.jar -s %s:%s -auth %s:%s declarative - linter < %s" \
+    command = "java -jar jenkins-cli.jar -s http://%s:%s -auth %s:%s declarative - linter < %s" \
               % (JENKINS_URL, JENKINS_PORT, JENKINS_USER, JENKINS_PASS, filename)
     stream = subprocess.Popen(command, shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = stream.communicate()[0].decode("utf-8")
@@ -39,7 +39,7 @@ def linter():
 def replay_job():
     build_nr = request.args.get("build_nr", default=0, type=int)
     filename = request.args.get("filename", default=JENKINS_DIR + "/Jenkinsfile", type=str)
-    command = " java -jar jenkins-cli.jar -s %s:%s -auth %s:%s replay-pipeline %s -n %i < %s" \
+    command = " java -jar jenkins-cli.jar -s http://%s:%s -auth %s:%s replay-pipeline %s -n %i < %s" \
               % (JENKINS_URL, JENKINS_PORT, JENKINS_USER, JENKINS_PASS, JENKINS_JOB, build_nr, filename)
     stream = subprocess.Popen(command, shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = stream.communicate()[0].decode("utf-8")
