4a65c62a1e8086d2d09eb86bb02858f075979b3e
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2022-02-06 17:21:08 +0000

    Fixed python file name in inject shell

--- inject-shell.sh
+++ inject-shell.sh
@@ -1,5 +1,5 @@
 BUILD=$(python getCurrentBuild.py)
 echo "Toggling build number" $(($BUILD+1))
-python toggleBuild -n $(($BUILD+1))
+python toggleBuild.py -n $(($BUILD+1))
 xterm -e "cd /var/lib/jenkins/workspace; sudo su -s /bin/bash jenkins"
-trap "python toggleBuild -n"$(($BUILD+1)) EXIT
+trap "python toggleBuild.py -n"$(($BUILD+1)) EXIT
