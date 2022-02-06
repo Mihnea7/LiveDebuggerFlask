1cd7af3206947836951638282a0c69b099701d5b
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2022-02-06 17:35:20 +0000

    Fixed retrieval of build due to wrong python version

--- inject-shell.sh
+++ inject-shell.sh
@@ -1,5 +1,5 @@
-BUILD=$(python getCurrentBuild.py)
+BUILD=$(python3 getCurrentBuild.py)
 echo "Toggling build number" $(($BUILD+1))
 python3 toggleBuild.py -n $(($BUILD+1))
 xterm -e "cd /var/lib/jenkins/workspace; sudo su -s /bin/bash jenkins"
-trap "python3 toggleBuild.py -n"$(($BUILD+1)) EXIT
+trap "python3 toggleBuild.py -n" $(($BUILD+1)) EXIT
