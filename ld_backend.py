bd70c39e706911ae30344a2450481a22d7a49f23
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2022-02-06 21:28:33 +0000

    Added shell request mechanism

--- ld_backend.py
+++ ld_backend.py
@@ -49,15 +49,17 @@ def replay_job():
     return output if output else err
 
 
+@app.route('/request-shell')
+def handle_inject_shell_req():
+    command = "./inject-shell.sh %s:%s" % (JENKINS_URL, JENKINS_PORT)
+    stream = subprocess.Popen(command, shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = stream.communicate()[0].decode("utf-8")
+    err = stream.communicate()[1].decode("utf-8")
+    return output if output else err
+
+
 def get_current_build_number():
     data = json.loads(requests.get
                       ("http://%s:%s/job/%s/api/json" % (JENKINS_URL, JENKINS_PORT, JENKINS_JOB))
                       .text)
     return data["builds"][0]["number"]
-
-
-
-
-
-
-
---
+++ request-inject-shell.sh
@@ -0,0 +1,1 @@
+curl http://$1:$2/request-shell
