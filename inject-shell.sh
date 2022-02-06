8cc1f8d4c3193bb52bbae4d295072d33d29c52d3
Mihnea Maldaianu <maldaianumihnea@yahoo.com>
2022-02-06 17:36:35 +0000

    Fixed EXIT trap in inject shell

--- inject-shell.sh
+++ inject-shell.sh
@@ -2,4 +2,4 @@ BUILD=$(python3 getCurrentBuild.py)
 echo "Toggling build number" $(($BUILD+1))
 python3 toggleBuild.py -n $(($BUILD+1))
 xterm -e "cd /var/lib/jenkins/workspace; sudo su -s /bin/bash jenkins"
-trap "python3 toggleBuild.py -n" $(($BUILD+1)) EXIT
+trap "python3 toggleBuild.py -n $(($BUILD+1))" EXIT
